7c523914b395394ea98cf29edb26b995d8c33517
Christopher Robinson <40131459+MidoriRobin@users.noreply.github.com>
2020-05-20 07:51:07 -0500

    update views classes, and forms. Adjust html files

--- app/forms.py
+++ app/forms.py
@@ -1,29 +1,24 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField
+from wtforms import *
 from wtforms.validators import DataRequired, EqualTo, Length
 
 # Set your classes here.
 
 
 class RegisterForm(Form):
-    name = TextField(
-        'Username', validators=[DataRequired(), Length(min=6, max=25)]
-    )
-    email = TextField(
-        'Email', validators=[DataRequired(), Length(min=6, max=40)]
-    )
-    password = PasswordField(
-        'Password', validators=[DataRequired(), Length(min=6, max=40)]
-    )
-    confirm = PasswordField(
-        'Repeat Password',
-        [DataRequired(),
-        EqualTo('password', message='Passwords must match')]
-    )
+    username = StringField('Username', validators=[DataRequired(), Length(min=6, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Length(min=6, max=40)])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    DOB = DateField('Date of Birth', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=40)])
+    confirm = PasswordField('Repeat Password',[DataRequired(),EqualTo('password', message='Passwords must match')])
+
+#class ProfileForm(Form):
 
 
 class LoginForm(Form):
-    name = TextField('Username', [DataRequired()])
+    email = TextField('Email', [DataRequired()])
     password = PasswordField('Password', [DataRequired()])
 
 
